d9e64482a6bec1721690cdf8e42e996570081e2a
Tomsz Pajewski <toma123@wp.pl>
2020-01-27 09:57:59 +0100

    first commit

---
+++ 00_first.java
@@ -0,0 +1,7 @@
+public class 00_first {
+	public static void main(String[] args) {
+		System.out.println("")
+		System.out.println("witaj")
+		System.out.println("")
+	}	
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Zabawy dla przypomnienia 
+---
+Proszę nie przeglądaj tych głupot. Wykasuje je niebawem
+---
+2020-01-28
